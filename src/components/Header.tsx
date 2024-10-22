5dbe7ce063b028ca92927aff6d6c43d56b465765
Daniel Meneses <danielgpmeneses@gmail.com>
2024-10-21 22:58:25 -0300

    fazendo modificações na pagina inicial

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,20 @@ import { UserIcon } from '@heroicons/react/20/solid';
 
 function Header() {
   return (
-    <header className='bg-primary-color text-light-color h-[10vh] flex items-center justify-center w-full'>
-        <Nav className="flex space-x-8 items-center">
-            <Nav.Link href="/">Início</Nav.Link>
-            <Nav.Link href="/projects">Projetos</Nav.Link>
-            <Nav.Link href="/Articles">Artigos</Nav.Link>
-            <Nav.Link href="/sobre">Sobre</Nav.Link>
-            <Nav.Link href="/faq">FAQ</Nav.Link>
-            <Nav.Link className="flex items-center" href="/logintest"><UserIcon className="h-5 w-5 me-2"/>Entrar</Nav.Link>
-            <Nav.Link className="flex items-center" href="/admin-projects">adm</Nav.Link>
-        </Nav>
+    <header className="bg-primary-color text-light-color h-[8vh] flex items-center justify-center w-full">
+      <Nav className="flex space-x-20 items-center">
+        <Nav.Link className="text-lg" href="/">Início</Nav.Link>
+        <Nav.Link className="text-lg" href="/projects">Projetos</Nav.Link>
+        <Nav.Link className="text-lg" href="/Articles">Artigos</Nav.Link>
+        <Nav.Link className="text-lg" href="/sobre">Sobre</Nav.Link>
+        <Nav.Link className="text-lg" href="/faq">FAQ</Nav.Link>
+        <Nav.Link className="text-lg flex items-center" href="/logintest">
+          <UserIcon className="h-5 w-5 me-2" />Entrar
+        </Nav.Link>
+        <Nav.Link className="text-lg flex items-center" href="/admin-projects">adm</Nav.Link>
+      </Nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
